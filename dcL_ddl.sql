5debfa5b60c88152137ab5084979e74e49ab26d9
Wonder0Maker <78822060+Wonder0Maker@users.noreply.github.com>
2021-03-15 19:03:49 +0200

    Add files via upload

--- dcL_ddl.sql
+++ dcL_ddl.sql
@@ -4,64 +4,55 @@ GO
 
 USE TestDb;
 GO
-CREATE SCHEMA TestSchema
-	CREATE TABLE TestTable(		Id			INT NOT NULL,
-							Name		VARCHAR(20),
-							IsSold		BIT,
-							InvoiceDate DATE	);
+CREATE SCHEMA TestSchema;
+GO
+
+CREATE TABLE TestSchema.TestTable(		Id			INT NOT NULL,
+										Name		VARCHAR(20),
+										IsSold		BIT,
+										InvoiceDate DATE	);
 GO
 
 INSERT INTO TestSchema.TestTable
-VALUES	(1,	'Boat',		1, '2020-11-08'),
-		(2,	'Auto',		0, '2020-11-09'),
-		(3,	'Plane', null, '2020-12-09');
+VALUES	(1,	'Boat',		1,		'2020-11-08'),
+		(2,	'Auto',		0,		'2020-11-09'),
+		(3,	'Plane',	null,	'2020-12-09');
 GO
 
-USE TestDb;
-GO
 EXEC sp_configure 'CONTAINED DATABASE AUTHENTICATION', 1;
 GO
 RECONFIGURE;
 GO
-USE [master]
+USE TestDb;
 GO
 ALTER DATABASE TestDb SET CONTAINMENT = PARTIAL;
 GO
 
-CREATE LOGIN TestUser 
-	WITH PASSWORD = 'MandrykA1608';
-
-USE TestDb;
-GO
-CREATE USER TestUser FOR LOGIN TestUser;
+CREATE USER TestUser WITHOUT LOGIN;
 GO
 
-GRANT CONNECT ON DATABASE::TestDb TO TestUser;
+GRANT CONNECT TO TestUser;
 GO
 EXECUTE AS USER = 'TestUser';
 GO
-SELECT	CURRENT_USER;
+SELECT	* 
+FROM	TestSchema.TestTable;
 GO
-
 
 REVERT;
 GO
 SELECT	CURRENT_USER;
 GO
 
-USE TestDb;
-GO
 SELECT	* 
 FROM	TestSchema.TestTable;
 GO
 
-GRANT SELECT ON OBJECT::TestSchema.TestTable TO TestUser;
+GRANT SELECT TO TestUser;
 GO
 EXECUTE AS USER = 'TestUser';
 GO
 
-USE TestDb;
-GO
 SELECT	* 
 FROM	TestSchema.TestTable;
 GO
@@ -69,4 +60,3 @@ GO
 REVERT;
 GO
 DROP USER TestUser;
-
