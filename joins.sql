f38387ec5378c24f5ddda31811aa3a3a211488c5
Wonder0Maker <78822060+Wonder0Maker@users.noreply.github.com>
2021-03-25 16:37:38 +0200

    Changed joins.sql

--- joins.sql
+++ joins.sql
@@ -19,16 +19,17 @@ SELECT	p.FirstName
 FROM	Person.Person p;
 GO
 
-SELECT	p.FirstName
+
+SELECT	MyTable.FirstName
+	,MyTable.LastName
+	,MyTable.JobTitle
+FROM	(SELECT p.FirstName
 	,p.LastName
-	,(SELECT e.JobTitle
-	FROM	HumanResources.Employee e
-	WHERE	p.BusinessEntityID = e.BusinessEntityID) 
-	AS	JobTitle
-FROM	Person.Person p
-JOIN	HumanResources.Employee AS e
-	ON	(p.BusinessEntityID = e.BusinessEntityID);
-GO
+	,e.JobTitle
+	FROM	Person.Person AS p
+	JOIN	HumanResources.Employee AS e
+	ON	(p.BusinessEntityID = e.BusinessEntityID)) 
+	AS MyTable;
 
 
 SELECT DISTINCT	
@@ -40,7 +41,7 @@ CROSS JOIN	HumanResources.Employee AS e;
 GO
 
 SELECT	COUNT(*) AS myCount
-FROM (SELECT DISTINCT	
+FROM	(SELECT DISTINCT	
 	p.FirstName
 	,p.LastName
 	,e.JobTitle
