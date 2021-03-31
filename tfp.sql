aa76d7399ecd965f9d128e2f443ff3f366eca040
Wonder0Maker <78822060+Wonder0Maker@users.noreply.github.com>
2021-03-31 13:23:05 +0300

    Changed file tfp.sql

--- tfp.sql
+++ tfp.sql
@@ -7,18 +7,17 @@ ON	HumanResources.Department
 AFTER	INSERT, UPDATE
 AS
 THROW	51000, 'you cannot change this table', 1;
-ROLLBACK TRANSACTION;
 GO
 
 -- INSERT INTO HumanResources.Department VALUES ('KA','D', '2008-04-30 00:00:00.000')
+select * from HumanResources.Department;
 
 -- query 2
 CREATE TRIGGER	notifier2
 ON	DATABASE
-AFTER	ALTER_TABLE
+FOR	ALTER_TABLE
 AS
 THROW	51000, 'you cannot change any table', 1;
-ROLLBACK TRANSACTION;
 GO
 
 -- ALTER TABLE	HumanResources.Shift 
@@ -26,13 +25,15 @@ GO
 
 -- query 3
 CREATE FUNCTION	dbo.ufnConcatStrings 
-( @Var1 VARCHAR(30)
-, @Var2 VARCHAR(30)
+( @Var1	VARCHAR(30)
+, @Var2	VARCHAR(30)
 )
-RETURNS	VARCHAR(61)
+RETURNS VARCHAR(61)
 AS 
 BEGIN
-	RETURN	(CONCAT_WS('-', @Var1, @Var2 ))
+	DECLARE	@ConcatStrings VARCHAR(61)
+	SELECT	@ConcatStrings = (CONCAT_WS('-', @Var1, @Var2 ))
+	RETURN	@ConcatStrings
 END;
 GO
 
@@ -52,7 +53,7 @@ RETURN
 	SELECT	e.* 
 	FROM	HumanResources.Employee AS e
 	JOIN	HumanResources.EmployeeDepartmentHistory AS edh
-	ON	e.BusinessEntityID = edh.BusinessEntityID
+		ON	e.BusinessEntityID = edh.BusinessEntityID
 	WHERE	edh.DepartmentID = @Id
 )
 ;
@@ -63,16 +64,17 @@ GO
 
 -- query 5
 CREATE PROCEDURE	Person.uspSearchByName
-	@Name	NVARCHAR(30) = N'%' 
+	@Name	VARCHAR(30) = NULL
 AS
 SELECT 
 	p.BusinessEntityId 
 	, p.FirstName
 	, p.LastName
 FROM	Person.Person AS p
-WHERE	FirstName LIKE	@Name
-	OR	LastName LIKE	@Name;
+WHERE	FirstName	LIKE	'%' + @Name + '%'
+	OR	LastName	LIKE	'%' + @Name + '%';
 GO
 
--- EXECUTE	Person.uspSearchByName N'%ess%'
+-- EXECUTE	Person.uspSearchByName 'ess'
+
 
